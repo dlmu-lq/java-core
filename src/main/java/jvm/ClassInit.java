a407597d97ef211d2bcfef55b683c0e91c227472
liqiang <18018968632@163.com>
2021-08-01 17:27:44 +0800

    类初始化

---
+++ src/main/java/jvm/ClassInit.java
@@ -0,0 +1,47 @@
+package jvm;
+
+
+/**
+ * @author liqiang04
+ * @description TODO
+ * @date 2021/7/28 12:50 下午
+ */
+public class ClassInit {
+
+    private String oa;
+
+    private String ob;
+
+    private Integer oc;
+
+    private Integer od;
+
+    private static ClassInit classInit;
+
+
+    private static final String b = new String("测试b");
+
+    private static final String a = "测试a";
+
+    private static final int c = 22;
+
+    private static final Integer d = 22;
+    static {
+        classInit = new ClassInit();
+    }
+
+    public ClassInit() {
+        oa = a;
+        ob = b;
+        oc = c;
+        od = d;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(classInit.oa);
+        System.out.println(classInit.ob);
+        System.out.println(classInit.oc);
+        System.out.println(classInit.od);
+    }
+
+}
